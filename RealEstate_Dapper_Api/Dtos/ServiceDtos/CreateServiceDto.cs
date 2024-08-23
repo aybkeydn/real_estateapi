e0c40179450b71105bc577596c258e05a08e9c51
aybkeydn <93129910+aybkeydn@users.noreply.github.com>
2024-08-23 21:04:58 +0300

    last

---
+++ RealEstate_Dapper_Api/Dtos/ServiceDtos/CreateServiceDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate_Dapper_Api.Dtos.ServiceDtos
+{
+    public class CreateServiceDto
+    {
+        
+
+        public string ServiceName { get; set; }
+
+        public bool ServiceStatus { get; set; }
+    
+}
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ServiceDtos;
+using RealEstate_Dapper_UI.Dtos.WhoWeAreDtos;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultWhoWeAreComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultWhoWeAreComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var client2 = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44375/api/WhoWeAreDetail");
+            var responseMessage2 = await client2.GetAsync("https://localhost:44375/api/Services");
+            if (responseMessage.IsSuccessStatusCode && responseMessage2.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultWhoWeAreDetailDto>>(jsonData);
+                var value2 = JsonConvert.DeserializeObject<List<ResultServiceDtos>>(jsonData2);
+                ViewBag.title = value.Select(x => x.Title).FirstOrDefault();
+                ViewBag.subTitle = value.Select(x => x.Title).FirstOrDefault();
+                ViewBag.description1 = value.Select(x => x.Title).FirstOrDefault();
+                ViewBag.description2 = value.Select(x => x.Title).FirstOrDefault();
+                return View(value2);
+            }
+            return View();
+        }
+    }
+}
